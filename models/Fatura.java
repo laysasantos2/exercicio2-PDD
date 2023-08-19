e149b74ab4791f97c322e48be80a8e0c7a1fd5f4
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-08-19 17:25:56 -0300

    Refactor Fatura and ProcessadorrBoleto

--- models/Fatura.java
+++ models/Fatura.java
@@ -1,18 +1,52 @@
 package models;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import utils.formataData;
 
+enum StatusPagamento {
+    PAGO,
+    NAO_PAGO
+}
+
 public class Fatura {
     private LocalDate data;
+    private double valorPago;
     private double valor;
+    private StatusPagamento statusPagamento;
+
     private String cliente;
+    private List<Pagamento> pagamentos;
 
     public Fatura(String data, double valor, String cliente) {
         this.data = formataData.formataDataStringToLocal(data);
         this.valor = valor;
         this.cliente = cliente;
+        this.pagamentos = new ArrayList<Pagamento>();
+        this.valorPago = 0;
+        this.statusPagamento = StatusPagamento.NAO_PAGO;
+    }
+
+    public void adicionarPagamento(Pagamento pagamento) {
+        pagamentos.add(pagamento);
+        this.valorPago += pagamento.getValor();
+        this.verificaPagamento();
+    }
+
+    public ArrayList<Pagamento> getPagamentos() {
+        return (ArrayList<Pagamento>) this.pagamentos;
+    }
+
+    private void verificaPagamento() {
+        if (this.valorPago >= valor) {
+            this.statusPagamento = StatusPagamento.PAGO;
+        }
+    }
+
+    public boolean isPaga() {
+        return this.statusPagamento == StatusPagamento.PAGO;
     }
 
     public String getData() {
--- models/ProcessadorBoleto.java
+++ models/ProcessadorBoleto.java
@@ -4,15 +4,9 @@ import java.util.List;
 
 public class ProcessadorBoleto {
     public static void processarFatura(Fatura fatura, List<Boleto> boletos) {
-        double totalPago = 0;
-
         for (Boleto boleto : boletos) {
-            totalPago += boleto.getValor();
-            fatura.adicionarPagamento(boleto.getValor(), boleto.getData(), "BOLETO");
-        }
-
-        if (totalPago >= fatura.valorTotal) {
-            fatura.marcarComoPaga();
+            Pagamento pagamento = new Pagamento(boleto.getValor(), boleto.getData(), TipoPagamento.BOLETO);
+            fatura.adicionarPagamento(pagamento);
         }
     }
 }
