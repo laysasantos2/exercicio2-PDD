9ee3b143e296cf38602fae33824a359c75972a99
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-08-19 15:51:08 -0300

    test: add tests for processadorBoleto

--- tests/ProcessadorBoletoTest.java
+++ tests/ProcessadorBoletoTest.java
@@ -1,13 +1,53 @@
 package tests;
 
-import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import models.Boleto;
+import models.Fatura;
 import models.ProcessadorBoleto;
 
 public class ProcessadorBoletoTest {
+    ProcessadorBoleto processadorBoleto = new ProcessadorBoleto();
+
     @Test
-    public void createProcessadorBoleto() {
-        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto();
-        
+    public void testProcessarFaturaNaoPaga() {
+        Fatura fatura = new Fatura("05/08/2023", 1500.00, "Cliente");
+        List<Boleto> boletos = new ArrayList<>();
+        boletos.add(new Boleto(001, "05/08/2023", 500.00));
+        boletos.add(new Boleto(002, "05/08/2023", 400.00));
+
+        processadorBoleto.processarFatura(fatura, boletos);
+
+        assertFalse(fatura.isPaga());
+    }
+
+    @Test
+    public void testProcessarFaturaPaga() {
+        Fatura fatura = new Fatura("05/08/2023", 1500.00, "Cliente");
+        List<Boleto> boletos = new ArrayList<>();
+        boletos.add(new Boleto(001, "05/08/2023", 500.00));
+        boletos.add(new Boleto(002, "05/08/2023", 400.00));
+        boletos.add(new Boleto(003, "05/08/2023", 600.00));
+
+        processadorBoleto.processarFatura(fatura, boletos);
+
+        assertTrue(fatura.isPaga());
+    }
+
+    @Test
+    public void testProcessarFaturaPagamentoCriado() {
+        Fatura fatura = new Fatura("05/08/2023", 1500.00, "Cliente");
+        List<Boleto> boletos = new ArrayList<>();
+        boletos.add(new Boleto(001, "05/08/2023", 500.00));
+
+        processadorBoleto.processarFatura(fatura, boletos);
+
+        assertEquals(1, fatura.getPagamentos().size());
+        assertEquals("BOLETO", fatura.getPagamentos().get(0).getTipo());
     }
 }
