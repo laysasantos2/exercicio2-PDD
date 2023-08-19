2df10c8ae6bd8fbdc40dc38d554f6f8282e3b22b
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-08-19 15:34:12 -0300

    Add tests for processarBoleto

---
+++ tests/ProcessadorBoletoTest.java
@@ -0,0 +1,13 @@
+package tests;
+
+import org.junit.Test;
+
+import models.ProcessadorBoleto;
+
+public class ProcessadorBoletoTest {
+    @Test
+    public void createProcessadorBoleto() {
+        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto();
+        
+    }
+}
