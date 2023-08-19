5c8986312b079ddd01ec4cb830c6f6b930fc0267
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-08-19 13:44:23 -0300

    test: add test to creating a bill object

--- tests/BillTest.java
+++ tests/BillTest.java
@@ -1,10 +1,20 @@
 package tests;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
+
+import models.Bill;
 
 public class BillTest {
     @Test
     public void createBillObject() {
+        Bill bill = Bill(0001, "05/07/2008", 500);
 
+        assertNotNull(bill);
+
+        assertEquals(bill.getCodigo(), 0001);
+        assertEquals(bill.getData(), "05/07/2008");
+        assertEquals(bill.getValor(). 500);
     }
 }
