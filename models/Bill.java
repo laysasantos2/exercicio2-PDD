71ac7904296a3b755998217c3c34c43da01e491e
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-08-19 14:25:35 -0300

    Test: add tests for the date formated in Bill

--- models/Bill.java
+++ models/Bill.java
@@ -1,5 +1,52 @@
 package models;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Bill {
-    
+    private int codigo;
+    private LocalDate data;
+    private double valor;
+
+    public Bill(int codigo, String data, double valor) {
+        this.codigo = codigo;
+        this.data = formataDataStringToLocal(data);
+        this.valor = valor;
+    }
+
+    private String formataDataLocalToString(LocalDate data) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return data.format(formatter);
+    }
+
+    private LocalDate formataDataStringToLocal(String data) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate returnData = LocalDate.parse(data, formatter);
+        return returnData;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public String getData() {
+        return this.formataDataLocalToString(data);
+    }
+
+    public double getValor() {
+        return this.valor;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setData(String data) {
+        this.data = formataDataStringToLocal(data);
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
 }
--- tests/BillTest.java
+++ tests/BillTest.java
@@ -1,6 +1,13 @@
 package tests;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 import org.junit.Test;
 
@@ -9,12 +16,32 @@ import models.Bill;
 public class BillTest {
     @Test
     public void createBillObject() {
-        Bill bill = Bill(0001, "05/07/2008", 500);
+        Bill bill = new Bill(0001, "05/07/2008", 500);
 
         assertNotNull(bill);
 
         assertEquals(bill.getCodigo(), 0001);
         assertEquals(bill.getData(), "05/07/2008");
-        assertEquals(bill.getValor(). 500);
+        assertEquals(bill.getValor(), 500);
+    }
+
+    @Test
+    public void createDataIncorrectly() {
+        try {
+            new Bill(1, "0585/0712/2008", 500);
+        } catch (Exception error) {
+            assertNotNull(error);
+        }
+    }
+
+    @Test
+    public void setDataIncorrectly() {
+        try {
+            Bill bill = new Bill(1, "05/08/2008", 500);
+            bill.setData("4343/212/434");
+            fail();
+        } catch (Exception error) {
+            assertNotNull(error);
+        }
     }
 }
