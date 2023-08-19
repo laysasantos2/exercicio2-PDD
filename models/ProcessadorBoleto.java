ff8ad79c145f0009f4b7b474b7d2cd3559cfac2b
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-08-19 15:47:13 -0300

    test: add code to processadorBoleto

--- models/ProcessadorBoleto.java
+++ models/ProcessadorBoleto.java
@@ -1,5 +1,18 @@
 package models;
 
+import java.util.List;
+
 public class ProcessadorBoleto {
-    
+    public static void processarFatura(Fatura fatura, List<Boleto> boletos) {
+        double totalPago = 0;
+
+        for (Boleto boleto : boletos) {
+            totalPago += boleto.getValor();
+            fatura.adicionarPagamento(boleto.getValor(), boleto.getData(), "BOLETO");
+        }
+
+        if (totalPago >= fatura.valorTotal) {
+            fatura.marcarComoPaga();
+        }
+    }
 }
