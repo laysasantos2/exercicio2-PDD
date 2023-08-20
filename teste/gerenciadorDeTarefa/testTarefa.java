f024e01a931f09714b07f22252020c41688ac4e3
Laysa <laysa.silva@ccc.ufcg.edu.br>
2023-08-20 13:19:29 -0300

    criando o teste atualizar titulo

--- teste/gerenciadorDeTarefa/testTarefa.java
+++ teste/gerenciadorDeTarefa/testTarefa.java
@@ -9,4 +9,11 @@ public class testTarefa {
     public void criarTarefa() {
         Tarefa t = new Tarefa("estudar concorrente", "semaforos", "18/08/2023", "média");
     }
+
+    @Test
+    public void atualizarTitulo() {
+        Tarefa t = new Tarefa("estudar concorrente", "semaforos", "18/08/2023", "média");
+        t.setTitulo("estudar IA");
+        assertEquals(t.getTitulo(), "estudar IA");
+    }
 }
