bba97d3dc1d50243690cc9fe0c21d6fb20ab26c1
Laysa <laysa.silva@ccc.ufcg.edu.br>
2023-08-20 13:11:24 -0300

    criando o teste criar tarefa

---
+++ teste/gerenciadorDeTarefa/testTarefa.java
@@ -0,0 +1,11 @@
+package gerenciadorDeTarefa;
+
+import org.junit.jupiter.api.Test;
+
+public class testTarefa {
+
+    @Test
+    public void criarTarefa() {
+        Tarefa t = new Tarefa("estudar concorrente", "semaforos", "18/08/2023", "média");
+    }
+}
