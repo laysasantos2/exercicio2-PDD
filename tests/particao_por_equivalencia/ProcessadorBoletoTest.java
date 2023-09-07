82d4bffffd3624bdd265fc1871eadbee40af1e7c
joaoabrantesuchoa <joao.abrantesuchoa@gmail.com>
2023-09-07 11:46:52 -0300

    test: add tests using the tecnique "partições por equivalência"

---
+++ tests/particao_por_equivalencia/ProcessadorBoletoTest.java
@@ -0,0 +1,51 @@
+package tests.particao_por_equivalencia;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import models.Boleto;
+import models.Fatura;
+import models.ProcessadorBoleto;
+
+public class ProcessadorBoletoTest {
+    @Test
+    public void testFaturaIgualSomaDosBoletos() {
+        Fatura fatura = new Fatura("05/08/2023", 500.00, "Cliente");
+        List<Boleto> boletos = new ArrayList<>();
+        boletos.add(new Boleto(001, "05/08/2023", 500.00));
+
+        ProcessadorBoleto.processarFatura(fatura, boletos);
+
+        assertFalse(fatura.isPaga());
+    }
+
+    @Test
+    public void testFaturaMenorQueSomaDosBoletos() {
+        Fatura fatura = new Fatura("05/08/2023", 499.00, "Cliente");
+
+        List<Boleto> boletos = new ArrayList<>();
+        boletos.add(new Boleto(001, "05/08/2023", 500.00));
+
+        ProcessadorBoleto.processarFatura(fatura, boletos);
+
+        assertTrue(fatura.isPaga());
+    }
+
+    @Test
+    public void testFaturaMaiorQueSomaDosBoletos() {
+
+        Fatura fatura = new Fatura("05/08/2023", 501.00, "Cliente");
+
+        List<Boleto> boletos = new ArrayList<>();
+        boletos.add(new Boleto(001, "05/08/2023", 500.00));
+
+        ProcessadorBoleto.processarFatura(fatura, boletos);
+
+        assertFalse(fatura.isPaga());
+    }
+}
